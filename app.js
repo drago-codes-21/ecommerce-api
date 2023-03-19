dd1f00428e49db49f7c896664ca35b03b1ee329c
Sartthhak <97092935+drago-codes-21@users.noreply.github.com>
2023-03-19 10:18:24 +0530

    small fix

--- app.js
+++ app.js
@@ -58,10 +58,10 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
+    await connectDB();
   } catch (error) {
     console.log(error);
   }
--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const connectDB = (url) => {
-  return mongoose.connect(url);
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected to database");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
